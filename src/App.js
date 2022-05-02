f0c1c8495cd57941832d97fa92cf6f6ef6008f18
topiaspe <topiaspe@metropolia.fi>
2022-05-02 12:23:37 +0300

    exercise 5.1 bloglist frontend, logged in text added

--- src/App.js
+++ src/App.js
@@ -35,7 +35,10 @@ const App = () => {
         <LoginForm handleLogin={handleLogin} setUsername={setUsername} setPassword={setPassword} username={username} password={password} /> :
         <div>
           <h2>blogs</h2>
-          {blogs.map(blog =>
+          <div> {user.name} logged in </div>
+          <br />
+          {
+          blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
         </div>
